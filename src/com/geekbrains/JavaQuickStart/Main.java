da0d56388d5e6d79892f6bc0981159c7cef1773b
EvgeniiKolesnikov <jekow@yandex.ru>
2020-10-16 19:48:21 +0300

    add Cycle

--- src/com/geekbrains/JavaQuickStart/Main.java
+++ src/com/geekbrains/JavaQuickStart/Main.java
@@ -3,25 +3,24 @@ package com.geekbrains.JavaQuickStart;
 import java.util.Scanner;
 
 public class Main {
-
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Ваша задача угадать число.");
-        int range = 100;
-        int number = (int) (Math.random() * range);
-        playLeve1(range, number);
+        for (int i = 10; i <= 30; i += 10) playLeve1(i);
+        System.out.println("Спасибо за игру!!!");
         scanner.close();
     }
 
-    private static void playLeve1(int range, int number) {
-        while(true) {
+    private static void playLeve1(int range) {
+        int number = (int) (Math.random() * range);
+        while (true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
             if (input_number == number) {
                 System.out.println("Вы угадали.");
                 break;
-            } else if(input_number > number) {
+            } else if (input_number > number) {
                 System.out.println("Загаданное число меньше");
             } else {
                 System.out.println("Загаданное число больше");
