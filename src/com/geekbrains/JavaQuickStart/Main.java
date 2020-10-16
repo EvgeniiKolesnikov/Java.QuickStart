cd285e4c0dc2d32afaaff34aae61138a04f7f812
EvgeniiKolesnikov <jekow@yandex.ru>
2020-10-16 19:38:54 +0300

    add method

--- src/com/geekbrains/JavaQuickStart/Main.java
+++ src/com/geekbrains/JavaQuickStart/Main.java
@@ -3,24 +3,29 @@ package com.geekbrains.JavaQuickStart;
 import java.util.Scanner;
 
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Ваша задача угадать число.");
         int range = 100;
         int number = (int) (Math.random() * range);
-        while (true) {
+        playLeve1(range, number);
+        scanner.close();
+    }
+
+    private static void playLeve1(int range, int number) {
+        while(true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
             if (input_number == number) {
                 System.out.println("Вы угадали.");
                 break;
-            } else if (input_number > number) {
+            } else if(input_number > number) {
                 System.out.println("Загаданное число меньше");
             } else {
                 System.out.println("Загаданное число больше");
             }
         }
-        scanner.close();
-        // hi hi
     }
 }
